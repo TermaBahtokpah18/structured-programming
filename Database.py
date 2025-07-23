6fa6464135a3415b5235f21335fcc2ee3ce4c684
TermaBahtokpah18 <termabahtokpah3@gmail.com>
2025-07-23 13:21:39 +0300

    Add files via upload

---
+++ Database.py
@@ -0,0 +1,42 @@
+#list data structure[]
+"""
+
+boys =['James',['John'],['Peter'],['Mark']]
+print(boys[0]) ##picking the first person.index = 0
+
+"""
+### adding elements of the list
+girls = []
+girls.append("Sarah")
+print(girls)
+
+
+
+## number = int(input("Enter the number of girls you want"))
+## girl = input("Enter the name")
+   # girl.append(girls)
+    #i +=1
+# print(girls) 
+
+
+ ### list operations
+girl = ['Sarah','Salia','Marlee','Sayla']
+print(girl)
+print(f"Original: {girl}")
+
+## list modifications
+girl[0] = "Sarliah"
+print(girl)
+girl.insert(1, "Salia")
+print(f"Original: {girl}")
+print(girl)
+
+girl.pop() ## deletes only the last name
+
+girl.remove("Salia")
+print(f"Removed Salia: {girl}")
+print(girl)
+
+
+
+ 
